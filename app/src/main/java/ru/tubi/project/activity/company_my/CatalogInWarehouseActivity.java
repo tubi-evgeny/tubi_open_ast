a5d905927141a81dac76dfc73d8503e925763ca8
Evgeny <kent7755@ya.ru>
2022-06-30 10:31:40 +0300

    Добавлен try - catch

--- app/src/main/java/ru/tubi/project/activity/company_my/CatalogInWarehouseActivity.java
+++ app/src/main/java/ru/tubi/project/activity/company_my/CatalogInWarehouseActivity.java
@@ -8,6 +8,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -140,36 +141,40 @@ public class CatalogInWarehouseActivity extends AppCompatActivity implements Vie
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void splitResult(String result){
         warehousesList.clear();
-        String [] res=result.split("<br>");
-        String[]one_temp = res[0].split("&nbsp");
-        if(one_temp[0].equals("error") || one_temp[0].equals("messege")){
-            Toast.makeText(this, ""+one_temp[1], Toast.LENGTH_LONG).show();
-            return;
-        }else{
-            for(int i=0;i<res.length;i++){
-                String[]temp = res[i].split("&nbsp");
+        try {
+            String[] res = result.split("<br>");
+            String[] one_temp = res[0].split("&nbsp");
+            if (one_temp[0].equals("error") || one_temp[0].equals("messege")) {
+                Toast.makeText(this, "" + one_temp[1], Toast.LENGTH_LONG).show();
+                return;
+            } else {
+                for (int i = 0; i < res.length; i++) {
+                    String[] temp = res[i].split("&nbsp");
 
-                int warehouse_info_id = Integer.parseInt(temp[0]);
-                String city = temp[1];
-                String street = temp[2];
-                int house = Integer.parseInt(temp[3]);
-                String building="";
-                int warehouse_id = Integer.parseInt(temp[5]);
-                String warehouse_type = temp[6];
-                try {
-                    building = temp[4];
-                }catch (Exception ex){
+                    int warehouse_info_id = Integer.parseInt(temp[0]);
+                    String city = temp[1];
+                    String street = temp[2];
+                    int house = Integer.parseInt(temp[3]);
+                    String building = "";
+                    int warehouse_id = Integer.parseInt(temp[5]);
+                    String warehouse_type = temp[6];
+                    try {
+                        building = temp[4];
+                    } catch (Exception ex) {
+                    }
+                    WarehouseModel warehouse = new WarehouseModel(warehouse_info_id, city, street, house,
+                            building, warehouse_id, warehouse_type);
+
+                    warehousesList.add(warehouse);
                 }
-                WarehouseModel warehouse=new WarehouseModel(warehouse_info_id, city, street, house,
-                        building, warehouse_id, warehouse_type);
+                //сортируем лист по 2 полям (getWarehouse_type и getWarehouse_id)
+                warehousesList.sort(Comparator.comparing(WarehouseModel::getWarehouse_type)
+                        .thenComparing(WarehouseModel::getWarehouse_id));
 
-                warehousesList.add(warehouse);
+                adap.notifyDataSetChanged();
             }
-            //сортируем лист по 2 полям (getWarehouse_type и getWarehouse_id)
-            warehousesList.sort(Comparator.comparing(WarehouseModel::getWarehouse_type)
-                    .thenComparing(WarehouseModel::getWarehouse_id));
-
-            adap.notifyDataSetChanged();
+        }catch(Exception ex){
+            Log.d("A111","CatalogInWarehouseActivity / splitResult result="+result);
         }
     }
 
