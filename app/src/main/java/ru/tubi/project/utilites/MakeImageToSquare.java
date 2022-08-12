dd65efddf03adfc3ff0b87e831af13ed8a163900
Evgeny <kent7755@ya.ru>
2022-08-12 09:20:06 +0300

    Добавлена обработка ошибки пустого bitmap

--- app/src/main/java/ru/tubi/project/utilites/MakeImageToSquare.java
+++ app/src/main/java/ru/tubi/project/utilites/MakeImageToSquare.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.util.Log;
 import android.widget.ImageView;
 
+import ru.tubi.project.R;
+
 public class MakeImageToSquare {
     private Context context;
     private ImageView imageView;
@@ -12,18 +14,33 @@ public class MakeImageToSquare {
     public MakeImageToSquare(Context context, Bitmap bitmap, ImageView imageView){
         this.context=context;
         this.imageView=imageView;
+        try{
+            int bmt_height  = bitmap.getHeight();
 
-        Bitmap resizedbitmap = makeBitmap(bitmap);
+            Bitmap resizedbitmap = makeBitmap(bitmap);
 
-        this.imageView.setImageBitmap(resizedbitmap);
+            this.imageView.setImageBitmap(resizedbitmap);
+        }catch(Exception ex){
+            this.imageView.setImageResource(R.drawable.tubi_logo_no_image_300ps);
+            Log.d("A111","MakeImageToSquare / exception = "+ex);
+            Log.d("A111","MakeImageToSquare / ERROR  bitmap isEmpty");
+        }
+
     }
     //задаем размер фото для квадрата и показываем
     public MakeImageToSquare(Bitmap bitmap, ImageView imageView){
         this.imageView=imageView;
-            //Log.d("A111","MakeImageToSquare / test 1");
-        Bitmap resizedbitmap = makeBitmap(bitmap);
+        try{
+            int bmt_height  = bitmap.getHeight();
 
-        this.imageView.setImageBitmap(resizedbitmap);
+            Bitmap resizedbitmap = makeBitmap(bitmap);
+
+            this.imageView.setImageBitmap(resizedbitmap);
+        }catch(Exception ex){
+            this.imageView.setImageResource(R.drawable.tubi_logo_no_image_300ps);
+            Log.d("A111","MakeImageToSquare 2 / exception = "+ex);
+            Log.d("A111","MakeImageToSquare 2 / ERROR  bitmap isEmpty");
+        }
     }
 
     private Bitmap makeBitmap(Bitmap bitmap){
@@ -41,7 +58,6 @@ public class MakeImageToSquare {
         //новый bitmap обрезаем в квадрат
         Bitmap resizedbitmap=Bitmap.createBitmap(
                 bitmap,x,y, finish_size, finish_size);
-
         return resizedbitmap;
     }
 }
